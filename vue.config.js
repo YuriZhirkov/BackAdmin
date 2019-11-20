57c777598723efdb8506986c63c92c9691a69520
YuriZhirkov <1562033492@qq.com>
2019-11-20 09:50:51 +0800

    迁移

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,7 @@ let externals = {
 };
 module.exports = {
   //基本路径
-  publicPath: "/dist/",
+  publicPath: "/",
   //输出文件目录
   outputDir: "dist",
   //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
