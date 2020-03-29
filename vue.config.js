83f6293acc3c3f313ac07067d0a3b7c0712a3ae2
YuriZhirkov <1562033492@qq.com>
2020-03-29 10:58:02 +0800

    跑起来

--- vue.config.js
+++ vue.config.js
@@ -123,7 +123,7 @@ module.exports = {
   devServer: {
     port: 8080, // 端口
     open: true, // 自动开启浏览器
-    compress: false, // 开启压缩
+    compress: true, // 开启压缩
     overlay: {
       warnings: true,
       errors: true
